267ba9cdd06222266b1d6fa9c2e06f16a00abf0f
Carlos Armada <Carlos Armada>
2017-06-28 20:03:37 -0400

    Added a Procfile.

---
+++ app.js
@@ -0,0 +1,16 @@
+var bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/',function(req, res) {
+	res.send('Hellos World!')
+	})
+app.post('/readings',function(req, res){
+    console.log(req.body.test);
+    res.send(req.body.test.level2 +'Hello');
+})
+app.listen(3000,function () {
+	console.log('Example app listening on port 3000!')
+	})
