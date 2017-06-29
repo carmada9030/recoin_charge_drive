2d88a1567ad09ed855e795c02e158618974cca19
Carlos Armada <Carlos Armada>
2017-06-28 20:57:29 -0400

    comment out .listen

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.post('/readings',function(req, res){
     console.log(req.body.test);
     res.send(req.body.test.level2 +'Hello');
 })
-app.listen(process.env.PORT || 3000) {
-	console.log('Example app listening on port 3000!')
-	})
+//app.listen(process.env.PORT || 3000) {
+//	console.log('Example app listening on port 3000!')
+//	})
